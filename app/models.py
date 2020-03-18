4df61a0ec353d875176237c4b9c42f07dbad182b
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2020-03-18 17:12:02 +0300

    Completed user model unit test

--- app/models.py
+++ app/models.py
@@ -32,3 +32,13 @@ class User(db.Model, TimestampMixin):
         salt = gen_salt(32)
         self.salt = salt
         self.hashed_password = generate_password_hash(password + salt)
+
+    def verify_password(self, password):
+        return check_password_hash(self.hashed_password, password + self.salt)
+
+    def json(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'created_at': str(self.created_at),
+        }
--- tests/unit/test_user_model.py
+++ tests/unit/test_user_model.py
@@ -12,6 +12,7 @@ def test_new_user(new_user):
     THEN check the username and hashed_password fields are defined correctly
     """
     user, _ = new_user
+
     assert_that(repr(user)).matches(r"\<User '[\w_-]+?'\>")
     assert_that(user.salt).is_length(32)
     assert_that(len(user.hashed_password)).is_greater_than(50)
@@ -24,6 +25,7 @@ def test_that_correct_password_returns_true(new_user):
     THEN check that a correct password returns TRUE
     """
     user, user_data = new_user
+
     assert_that(user.verify_password(user_data.get('password'))).is_true()
 
 
@@ -33,8 +35,10 @@ def test_that_an_incorrect_password_returns_false(new_user):
     WHEN a new User is created
     THEN check that an incorrect password returns FALSE
     """
-    user, _ = new_user
-    assert_that(user.verify_password('xxddxx')).is_false()
+    user, user_data = new_user
+    password = 'x{}x'.format(user_data.get('password'))
+
+    assert_that(user.verify_password(password)).is_false()
 
 
 def test_json_representation_of_user(new_user):
@@ -45,7 +49,10 @@ def test_json_representation_of_user(new_user):
     """
     user, user_data = new_user
     user_json = user.json()
+
+    acceptable_keys = 'id', 'username', 'created_at'
+    unacceptable_keys = 'salt', 'password', 'hashed_password'
+
     assert_that(user_json).contains_value(user_data.get('username'))
-    assert_that(user_json).contains_key('id', 'username', 'created_at')
-    assert_that(user_json).does_not_contain(
-        'salt', 'password', 'hashed_password')
+    assert_that(user_json).contains_key(*acceptable_keys)
+    assert_that(user_json).does_not_contain_key(*unacceptable_keys)
