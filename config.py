56875cc2ffbe04797793e0ff39011a1fc979b34e
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2020-05-02 16:33:26 +0300

    sqlalchemy_echo for development

--- config.py
+++ config.py
@@ -28,6 +28,7 @@ class DevelopmentConfig(Config):
 
     ENV = 'development'
     DEBUG = True
+    SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 
