90a92c7b9f1261c57c76decdd85c123c09dfaeff
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2020-05-05 16:31:12 +0300

    frontend setup

---
+++ ui/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ['plugin:vue/essential', '@vue/airbnb'],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        indent: ['error', 4],
+        'vue/html-indent': ['error', 4],
+        'vue/script-indent': ['error', 4],
+        'arrow-parens': ['error', 'always'],
+    },
+};
